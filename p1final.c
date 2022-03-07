9c1f3eeec53e66213d7d5772f7f7ec7fe6c4dc48
Srinidhi R Bharadwaj <96413310+Srinidhirb@users.noreply.github.com>
2022-03-07 06:54:29 +0000

    1234

--- p1final.c
+++ p1final.c
@@ -1,1 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("Enter the fraction number:");
+  scanf("%d%d%d%d",num1,den1,num2,den2);
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  *num3=(num1*den2)+(num2*den1);
+  *den3=(den1*den2);
+  int i;
+   for(i=2;i<=*den3;i++)
+     {
+       if(*num3%i==0 && *den3==0)
+       {
+         *num3=*num3/i;
+         *den3=*den3/i;
+       }
+     }
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf("the sum of %d/%d and %d/%d is %d/%d",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+ int a,b,c,d,num,den;
+  input(&a,&b,&c,&d);
+  add(a,b,c,d,&num,&den);
+  output(a,b,c,d,num,den);
+  return 0;
+}
--- p3final.c
+++ p3final.c
@@ -1,48 +1,33 @@
-/*
-Write a program find whether a given number is a prime number.
-int input_number();
-int is_prime(int n);
-void output(int n, int is_prime);
-*/
-
-#include <stdio.h>
-#include <math.h>
-int input()
-{
-  int n;
-  printf("Enter the number\n");
-  scanf("%d",&n);
-  return n;
+nclude <stdio.h>
+void input(int *n,int *r)
+{#
+printf("enter the value of n and r:\n");
+scanf("%d%d",n, r);
+  }
+  int fact(int n) {
+  int res = 1;
+  for (int i = 2; i <= n; i++) {
+    res = res * i;
+  }
+  return res;
 }
-
-int isprime(int n)
-{
-  if (n==0 || n==1) {
-    return 0
-  }
-  if (n==2) {
-    return 1;
-  }
-  for(int i= 2; i<= sqrt(n);i++)
+  
+ int nCr(int n, int r)
   {
-    if(n%i==0)
-      return 0;
-  }
-  return 1;
-}
-void output(int n, int isp)
-{
-  if(isp == 0)
-    printf("%d is not a prime number\n",n);
-  else
-    printf("%d is a prime number\n",n)
-}
-
+    int result;
+    result= fact(n) / (fact(r) * fact(n - r)); 
+    return result;
+    }
+  void output (int n,int r,int result)
+  {
+  printf("value of %dC%d= %d\n",n,r,result );
+    }
+    
 int main()
-{
-  int n = input();
-  int isp = isprime(n);
-  output(n,isp);
+  {
+    int n,r,result;
+  input (&n,&r);
+  result=nCr(n,r);
+  output(n,r,result);
   return 0;
-  }
-}
+    }
